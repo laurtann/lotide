ae0e47848535a64c15d7f07d362450f0dd07080e
Laura Tannahill <lntannahill@gmail.com>
2020-10-26 18:39:48 +0000

    created assertEqual for Lotide Library

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💁💁💁 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🙅‍♀️🙅‍♀️🙅‍♀️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(3, 1);
