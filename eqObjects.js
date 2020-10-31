c04db373688ba86b2853e6446db68df1cabd3cbf
Laura Tannahill <lntannahill@gmail.com>
2020-10-31 01:09:39 +0000

    updated eqObjects to allow for nested objects with recursion. Added add'l test conditions.

--- eqObjects.js
+++ eqObjects.js
@@ -36,12 +36,15 @@ const eqObjects = function(object1, object2) {
       if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
+    } else if (typeof object1[key] === "object" && typeof object2[key] === "object") {
+      eqObjects(object1[key], object2[key]);
     } else if (object1[key] !== object2[key]) {
       return false;
     }
   }
   return true;
 };
+
 
 // tests
 
@@ -63,3 +66,27 @@ const eqObjects = function(object1, object2) {
 // const cd2 = { c: "1", d: ["2", 3, 4] };
 // eqObjects(cd, cd2); // => false
 // assertEqual(eqObjects(cd, cd2), false);
+
+// day 5 tests with recursion
+
+// const case1 = { a: { z: 1 }, b: 2 };
+// const case2 = { a: { z: 1 }, b: 2 };
+// eqObjects(case1, case2); //==> true
+// assertEqual(eqObjects(case1, case2), true);
+
+// const num1 = { a: { y: 0, z: 1 }, b: 2 };
+// const num2 =  { a: { z: 1 }, b: 2 } ;
+// eqObjects(num1, num2); // => false
+// assertEqual(eqObjects(num1, num2), false);
+
+// const like1 = { likes: { drinks: "pop" }, snacks: "chips" };
+// const like2 = { likes: { drinks: "pop" }, snacks: "chips" };
+// eqObjects(like1, like2) // => true
+// assertEqual(eqObjects(like1, like1), true);
+
+// const fruit1 = { fruits: { mangos: 6, bananas: 9 }, water: "sparkling" };
+// const fruit2 =  { fruits: { mangos: 6 }, bananas: 9, water: "sparking" };
+// eqObjects(fruit1, fruit2) // => false
+// assertEqual(eqObjects(fruit1, fruit2), false);
+
+
